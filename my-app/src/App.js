67bee5650284ccc9f726aebdfe87ab2405fd9969
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-28 14:24:51 -0700

    Add link to tutorial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
 
+//Follow along at: https://www.youtube.com/watch?v=E1E08i2UJGI
+
 function App() {
   return (
     <div className="todo-app">
